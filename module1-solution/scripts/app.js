a943655e9a59d7a9c446a9a0747ee24aeba6bbe0
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-04-20 14:34:35 +0200

    Implement the required functionality

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -6,7 +6,26 @@ angular.module('LunchCheck', [])
 
 LunchController.$inject = ['$scope'];
 function LunchController($scope) {
-  $scope.instruction = "List comma separated items that you usually have for lunch";
+    $scope.instruction = "List comma separated items that you usually have for lunch";
+    $scope.ingredients = "";
+    $scope.message = "";
+
+    $scope.getMessage = function () {
+        if ($scope.ingredients.length == 0) {
+            $scope.message = "Please enter data first";
+        } else {
+            if (countIngredients($scope.ingredients) > 3) {
+                $scope.message = "Too much!";
+            } else {
+                $scope.message = "Enjoy!";
+            }
+        }
+  }
+  
+    function countIngredients(string) {
+        var ingredients = string.split(",");
+        return ingredients.length;
+    }
 }
 
 })();
