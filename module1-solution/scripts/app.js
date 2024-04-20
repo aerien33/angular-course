13cce6ef38f8454e6b8528e6b2e1bf2364cc2915
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-04-20 12:28:04 +0200

    Protect dependency injection from minification

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -2,9 +2,11 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchController', LunchController);
 
-.controller('LunchController', function ($scope) {
-  $scope.instruction = "List comma separated items you usually have for lunch";
-});
+LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  $scope.instruction = "List comma separated items that you usually have for lunch";
+}
 
 })();
